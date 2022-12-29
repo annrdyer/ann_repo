2875efbf69cc10777a0f035f6c0aa80a88b7dd23
Ann <annrdyer@comcast.net>
2022-12-28 16:33:11 -0800

    Week 8 Assignment Complete

--- assignments/week-06-supervised-ml/nb/tpot_electronics_pipeline.py
+++ assignments/week-06-supervised-ml/nb/tpot_electronics_pipeline.py
@@ -1,9 +1,12 @@
 import numpy as np
 import pandas as pd
 from sklearn.decomposition import FastICA
-from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline
+from sklearn.pipeline import make_pipeline, make_union
+from sklearn.preprocessing import MinMaxScaler
+from tpot.builtins import StackingEstimator
+from xgboost import XGBClassifier
 from tpot.export_utils import set_param_recursive
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
@@ -14,8 +17,10 @@ training_features, testing_features, training_target, testing_target = \
 
 # Average CV score on the training set was: 1.0
 exported_pipeline = make_pipeline(
-    FastICA(tol=0.30000000000000004),
-    ExtraTreesClassifier(bootstrap=True, criterion="entropy", max_features=0.9000000000000001, min_samples_leaf=2, min_samples_split=5, n_estimators=100)
+    MinMaxScaler(),
+    FastICA(tol=0.35000000000000003),
+    StackingEstimator(estimator=XGBClassifier(learning_rate=0.01, max_depth=10, min_child_weight=12, n_estimators=100, n_jobs=1, subsample=1.0, verbosity=0)),
+    RandomForestClassifier(bootstrap=True, criterion="gini", max_features=0.2, min_samples_leaf=8, min_samples_split=4, n_estimators=100)
 )
 # Fix random state for all the steps in exported pipeline
 set_param_recursive(exported_pipeline.steps, 'random_state', 42)
--- assignments/week-07-intro-dl/nb/fuel-efficiency-prediction-streamlit.py
+++ assignments/week-07-intro-dl/nb/fuel-efficiency-prediction-streamlit.py
@@ -1,9 +1,14 @@
 import pandas as pd
+import numpy as np
 import plotly.express as px
 import plotly.figure_factory as ff
 import streamlit as st
 import streamlit.components.v1 as components
 from PIL import Image
+
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_absolute_error
 
 # Add and resize an image to the top of the app
 img_fuel = Image.open("../img/fuel_efficiency.png")
@@ -12,8 +17,13 @@ st.image(img_fuel, width=700)
 st.markdown("<h1 style='text-align: center; color: black;'>Fuel Efficiency</h1>", unsafe_allow_html=True)
 
 # Import train dataset to DataFrame
-train_df = pd.read_csv("../dat/train.csv.gz", compression="gzip")
-model_results_df = pd.read_csv("../dat/model_results.csv")
+#train_df = pd.read_csv("../dat/train.csv.gz", compression="gzip")
+train_df = pd.read_csv('/home/annrdyer/ann_repo/assignments/week-07-intro-dl/nb/train_dataset.csv')
+train_df['Country'] = np.where(train_df['Europe']==1, 'Europe', 'Other')
+train_df['Country'] = np.where(train_df['USA']==1, 'USA', train_df['Country'])
+train_df['Country'] = np.where(train_df['Japan']==1, 'Japan', train_df['Country'])
+
+model_results_df = pd.read_csv('/home/annrdyer/ann_repo/assignments/week-07-intro-dl/nb/results.csv')
 
 # Create sidebar for user selection
 with st.sidebar:
@@ -23,7 +33,7 @@ with st.sidebar:
     # Available models for selection
 
     # YOUR CODE GOES HERE!
-    models = ["DNN", "TPOT"]
+    models = ['Linear Regression', "DNN", "TPOT"]
 
     # Add model select boxes
     model1_select = st.selectbox(
@@ -33,11 +43,14 @@ with st.sidebar:
     
     # Remove selected model 1 from model list
     # App refreshes with every selection change.
-    models.remove(model1_select)
+    if model1_select == 'Linear Regression' : models2 = ["DNN", "TPOT"]
+    elif model1_select == 'DNN' : models2 = ["Linear Regression", "TPOT"]
+    elif model1_select == 'TPOT' : models2 = ["Linear Regression", "DNN"]
+        
     
     model2_select = st.selectbox(
         "Choose Model 2:",
-        (models)
+        (models2)
     )
 
 # Create tabs for separation of tasks
@@ -56,10 +69,50 @@ with tab1:
     corr = train_df.corr()
     fig = px.imshow(corr)
     st.write(fig)
+    
+    
+    import plotly.express as px
+    
+    fig = px.scatter(train_df, 
+                     x="Displacement", 
+                     y="MPG", 
+                     color="Horsepower",
+                     hover_data=['Weight'], facet_col="Cylinders", facet_row="Country")
+    
+    st.plotly_chart(fig, use_container_width=True)
+    
 
 with tab2:    
     
     # YOUR CODE GOES HERE!
+    st.dataframe(model_results_df)
+    
+    expected = model_results_df['MPG'].tolist()
+    
+    if model1_select == 'Linear Regression':
+        predicted1 = model_results_df['Linear Regression'].tolist()
+    elif model1_select == 'DNN':  
+        predicted1 = model_results_df['DNN'].tolist()
+    elif model1_select == 'TPOT':  
+        predicted1 = model_results_df['TPOT'].tolist()                                  
+    
+         
+    if model2_select == 'Linear Regression':
+        predicted2 = model_results_df['Linear Regression'].tolist()
+    elif model2_select == 'DNN':  
+        predicted2 = model_results_df['DNN'].tolist()
+    elif model2_select == 'TPOT':  
+        predicted2 = model_results_df['TPOT'].tolist()                                 
+                                     
+                                            
+    rmse1 = mean_squared_error(expected, predicted1, squared=False)
+    rmse2 = mean_squared_error(expected, predicted2, squared=False)                                   
+    
+    mae1 = mean_absolute_error(expected, predicted1)
+    mae2 = mean_absolute_error(expected, predicted2)
+    
+    rsquared1 = r2_score(expected, predicted1)
+    rsquared2 = r2_score(expected, predicted2)                                   
 
     # Columns for side-by-side model comparison
     col1, col2 = st.columns(2)
@@ -69,14 +122,18 @@ with tab2:
         st.header(model1_select)
 
         # YOUR CODE GOES HERE!
-
+        st.write('RMSE: '+ str(rmse1))                             
+        st.write('MAE: ' + str(mae1))                                 
+        st.write('R Square: ' + str(rsquared1))     
 
     # Build confusion matrix for second model
     with col2:
         st.header(model2_select)
 
         # YOUR CODE GOES HERE!
-
+        st.write('RMSE: ' + str(rmse2))                            
+        st.write('MAE: ' + str(mae2))                                 
+        st.write('R Square: ' + str(rsquared2))  
 
 with tab3: 
     # YOUR CODE GOES HERE!
